7420aa8f4bf6305a7e1057728611003c8c8bbab8
Miha Jamsek <miha_jamsek@windowslive.com>
2019-03-31 18:37:19 +0200

    added filter for security headers

---
+++ api/src/main/java/com/mjamsek/tasker/filters/SecurityHeadersFilter.java
@@ -0,0 +1,31 @@
+package com.mjamsek.tasker.filters;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class SecurityHeadersFilter implements Filter {
+    
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    
+    }
+    
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        if (response instanceof HttpServletResponse) {
+            HttpServletResponse resp = (HttpServletResponse) response;
+            resp.addHeader("X-Content-Type-Options", "nosniff");
+            resp.addHeader("X-XSS-Protection", "1; mode=block");
+            resp.addHeader("X-Frame-Options", "DENY");
+            chain.doFilter(request, resp);
+        } else {
+            chain.doFilter(request, response);
+        }
+    }
+    
+    @Override
+    public void destroy() {
+    
+    }
+}
